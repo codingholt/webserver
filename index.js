fc679516c23fea3089f41575544854ce6d13bfa9
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-11-02 15:23:36 +0100

    made async

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const express = require('express');
-const {readFile} = require('fs')
+const {readFile} = require('fs').promises
 const app = express();
 
-app.get('/', (request, response) => {
-
-    readFile('./home.html', 'utf8', (err, html) => {
-
-        if (err){
-            response.status(500).send('sorry, out of order')
-        }
-        
-        response.send(html)
-
-    })
-
-
-});
+app.get('/', async(request, response) => {
+        response.send(await readFile('./home.html', 'utf8') )
+    });
 
 app.listen(process.env.PORT || 3000, () => console.log('App avaiable on http://localhost:3000'))
 
