92f8a6434ccf4fb4753795fb29efc6f968c74829
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-11-04 20:08:32 +0100

    added comments

--- routes/doggo.js
+++ routes/doggo.js
@@ -1,9 +1,12 @@
+//requirements
 const express = require('express');
 const router = express.Router();
 
-
+//on /doggo
 router.get('/', (req, res) => {
+    //renders doggo.ejs
     res.render('doggo')
 })
 
+//export to router
 module.exports = router
--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,29 @@
+//requirements
 const express = require('express');
 const router = express.Router();
 
+//basic home page doesnt make any sense
 router.get('/', (req, res) => {
     res.send('user list')
 })
 
-
+// '/new' page to make new user doesnt do anything
 router.get('/new', (req, res) => {
     res.send('user new form')
 })
-router.post('/', (req, res) => {
-    res.send('create user')
-})
+
 
 router
     .route("/:id")
+    //get route for id
     .get((req, res) => {
         res.send(`Get user with ID ${req.params.id}`)
     })
+    //put route for id
     .put((req, res) => {
         res.send(`update user with ID ${req.params.id}`)
     })
+    //delete route for id
     .delete((req, res) => {
         res.send(`Delete user with ID ${req.params.id}`)
     })
@@ -28,8 +31,13 @@ router
 const users = [{name: 'cooldude'}, {name: 'coolegast'}]
 
 router.param("id", (req, res, next, id) => {
+    //check id with position in array
     req.user = users[id]
+    //Log user if is present in the array
     console.log(req.user)
+
     next()
 })
+
+//export to router
 module.exports = router
