1b831eb0afbac68152680395f48dedbf84f6a87d
SvenNijholt <75948709+SvenNijholt@users.noreply.github.com>
2021-11-04 20:08:09 +0100

    added comments

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
+//requirements
 const express = require('express')
+//init express
 const app = express()
 
+//set view engine to ejs
 app.set('view engine', 'ejs')
 
+//set homepage
 app.get('/', (req, res) => {
     res.render('index')    
 })
 
 //routers
 const userRouter = require('./routes/user')
+const doggoRouter = require('./routes/doggo');
 
 app.use('/user', userRouter);
-
-const doggoRouter = require('./routes/doggo');
 app.use('/doggo', doggoRouter);
 
-app.use('/doggo', doggoRouter);
+
+//listen
 const port = 3000
 app.listen(port, console.log(`Running on: http://localhost:${port}`))
